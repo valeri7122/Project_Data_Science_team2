41f13dc09c144bb2cd0c41454b1082d91ca6565a
Valeri <110026147+valeri7122@users.noreply.github.com>
2023-08-02 02:50:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,5 +44,5 @@ async def predict_api(file: UploadFile = File(...)):
     return HTMLResponse(content=pred)
 
 
-#if __name__ == '__main__':
-    #uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
