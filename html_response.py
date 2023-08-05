eaf19f67df0ad89f94d24836ec7c164f027b8322
Valeri <110026147+valeri7122@users.noreply.github.com>
2023-08-06 02:24:59 +0300

    Add files via upload

---
+++ html_response.py
@@ -0,0 +1,18 @@
+def response(filename, prediction):
+    html = f"""
+        <html>
+            <head>
+                <title>Image prediction</title>
+                <link rel="stylesheet" href="/static/styles/styles2.css">
+            </head>
+	        <body>
+            	<br/>
+            	<input type="button" onclick="history.back();" value="Повернутись на головну сторінку"/>
+			    <p align="center"><img class="pos2" src="/static/upload/{filename}"></p>
+		        <h2 align="center">{prediction}</h2>
+            </body>
+        </html>
+        """
+
+    return html
+    
